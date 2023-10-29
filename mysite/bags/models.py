384f17ed536bdffc594c554d6527bf49e9e049e5
Kirmen <106472439+Kirmen@users.noreply.github.com>
2023-10-29 22:24:48 +0200

    messages

--- mysite/bags/models.py
+++ mysite/bags/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -40,3 +41,10 @@ class Category(models.Model):
         verbose_name = 'Категорія'
         verbose_name_plural = 'Категорії'
         ordering = ['title']
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
